6ac973ffbe05e154184ab40ef65e3d9c7a655736
Nikita <midirenr@gmail.com>
2023-07-20 00:04:34 +0300

    feat: added correlate_users_and_tasks function

--- creator.py
+++ creator.py
@@ -40,3 +40,31 @@ def validate_response(response) -> bool:
         return True
     else:
         return False
+
+
+def correlate_users_and_tasks(users: list[dict], tasks: list[dict]) -> None:
+    """
+    Установить соотношение между users и tasks
+
+    Ключевые аргументы:
+    users - список, каждый элемент которого является словарем с информацией о юзере
+    tasks - список, каждый элемент которого является словарем с информацией о задаче.
+    Каждая задача связана с юзером по ключу userId.
+
+    Описание:
+    - проходит циклом по каждому словарю в двух принятых в качестве аргумента списках;
+    - создает новые пары ключ-значение в словаре из списка users:
+    completed_task: словарь из списка tasks у которого completed=True
+    uncompleted_task: словарь из списка tasks у которого completed=False
+    - в случае совпадения user['id'] и task['userId'] добавляет task в user['completed_task'/'uncompleted_task']
+    """
+    for user in users:
+        user['completed_task'] = []
+        user['uncompleted_task'] = []
+        for task in tasks:
+            if user['id'] != task['userId']:
+                continue
+            if task['completed']:
+                user['completed_task'].append(task)
+            else:
+                user['uncompleted_task'].append(task)
