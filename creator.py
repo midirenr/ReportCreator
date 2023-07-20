1defafee14f20fea853eec090bf5e2eed9e1cf7d
Nikita <midirenr@gmail.com>
2023-07-20 11:31:15 +0300

    feat: added function for create_report

--- creator.py
+++ creator.py
@@ -162,3 +162,64 @@ def correlate_users_and_tasks(users: list[dict], tasks: list[dict]) -> None:
                 user['completed_task'].append(task)
             else:
                 user['uncompleted_task'].append(task)
+
+
+def create_report(data_list: list[dict]) -> None:
+    """
+    Создать отчет
+
+    Ключевые аргументы:
+    data_list - список, каждый элемент которого является словарем с информацией о юзере и его тасках
+
+    Описание:
+    - создает директорию tasks, если не находит ее;
+    - проходит циклом по каждому словарю в списке, где:
+        1) подготавливает данные к записи в файл (функция create_write_data)
+        2) получает из подготовленных данных дату и время формирования отчета (функция get_date_time_from_data)
+        3) ищет уже существующий отчет по username юзера:
+            3.1) в случае нахождения отчета:
+                3.1.1) сравнивает новые данные с данными существующего отчета;
+                3.1.2) если данные совпадают, то переходит к новой итерации;
+                3.1.3) если не совпадают, то переименовывает старый отчет и создает новый.
+            3.2) в случае отсутствия отчета создает новый
+    """
+    if not os.path.isdir('tasks'):
+        os.mkdir('tasks')
+
+    for data in data_list:
+        write_data = create_data_to_write(data)
+        date_time_of_write_data = get_date_time_from_data(write_data)
+
+        if os.path.exists(f'tasks/{data["username"]}.txt'):
+            file = open(f'tasks/{data["username"]}.txt', 'r')
+            file_data = file.read()
+            date_time_of_file_data = get_date_time_from_data(file_data)
+            file.close()
+
+            if write_data.replace(date_time_of_write_data, '') == file_data.replace(date_time_of_file_data, ''):
+                continue
+            else:
+                rename_old_report(f'{data["username"]}.txt', date_time_of_file_data)
+                new_file = open(f'tasks/{data["username"]}.txt', 'w')
+                new_file.write(write_data)
+                new_file.close()
+        else:
+            new_file = open(f'tasks/{data["username"]}.txt', 'w')
+            new_file.write(write_data)
+            new_file.close()
+
+
+if __name__ == "__main__":
+    # Отправляем запросы в API
+    response_tasks = send_request('https://json.medrocket.ru/todos')
+    response_users = send_request('https://json.medrocket.ru/users')
+
+    # Проверяем корректно ли выполнен запрос
+    if validate_response(response_users) and validate_response(response_tasks):
+        # Очищаем полученные данные
+        clearing_data_tasks = clearing_data_from_todos_api(response_tasks)
+        clearing_data_users = clearing_data_from_users_api(response_users)
+        # Устанавливаем соотношение между users и tasks
+        correlate_users_and_tasks(clearing_data_users, clearing_data_tasks)
+        # Создаем отчет
+        create_report(clearing_data_users)
